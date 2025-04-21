f16c6783e7834a1861976c110b33f9c3f367d491
wannaBpark <leehun456@hanmail.net>
2025-04-21 16:15:23 +0900

    Added - Shadow Render Pass: DepthOnlyVS, InputLayout, 기타 리소스 바인딩 호출 (StaticMeshRenderPass에서 Draw() 콜 호출 필요) wip : light 정보를 받아 알맞은 viewport width, height 바인딩 / Depth Map 정상적인 굽기

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/ShadowRenderPass.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/ShadowRenderPass.cpp
@@ -1,5 +1,10 @@
 #include "ShadowRenderPass.h"
 
+#include "Components/Light/LightComponent.h"
+#include "D3D11RHI/DXDBufferManager.h"
+#include "D3D11RHI/GraphicDevice.h"
+#include "D3D11RHI/DXDShaderManager.h"
+
 FShadowRenderPass::FShadowRenderPass()
 {
 }
@@ -8,14 +13,69 @@ FShadowRenderPass::~FShadowRenderPass()
 {
 }
 
+void FShadowRenderPass::Initialize(FDXDBufferManager* InBufferManager, FGraphicsDevice* InGraphics, FDXDShaderManager* InShaderManager)
+{
+    BufferManager = InBufferManager;
+    Graphics = InGraphics;
+    ShaderManager = InShaderManager;
+
+    // DepthOnly Vertex Shader
+    CreateShader();
+    UpdateViewport(ShadowMapWidth, ShadowMapHeight);
+}
+
+void FShadowRenderPass::PrepareRenderState()
+{
+    // Shader Hot Reload 대응 
+    StaticMeshIL = ShaderManager->GetInputLayoutByKey(L"StaticMeshVertexShader");
+    DepthOnlyVS = ShaderManager->GetVertexShaderByKey(L"DepthOnlyVS");
+    Graphics->DeviceContext->IASetInputLayout(StaticMeshIL);
+    Graphics->DeviceContext->VSSetShader(DepthOnlyVS, nullptr, 0);
+
+    // Note : PS만 언바인드할 뿐, UpdateLightBuffer에서 바인딩된 SRV 슬롯들은 그대로 남아 있음
+    Graphics->DeviceContext->PSSetShader(nullptr, nullptr, 0);
+    Graphics->DeviceContext->RSSetState(Graphics->RasterizerSolidBack);
+    UpdateViewport(ShadowMapWidth, ShadowMapHeight);
+}
+
 void FShadowRenderPass::PrepareRenderArr()
 {
 }
 
 void FShadowRenderPass::Render(const std::shared_ptr<FEditorViewportClient>& Viewport)
 {
+    //ShadowMapWidth = LightShadow->GetShadowWidth();
+    //ShadowMapHeight = LightShadow->GetShadowHeight();
+    PrepareRenderState();
+    
+
+
+    Graphics->DeviceContext->RSSetViewports(0, nullptr);
+    Graphics->DeviceContext->OMSetRenderTargets(0, nullptr, nullptr);
 }
 
 void FShadowRenderPass::ClearRenderArr()
 {
+}
+
+void FShadowRenderPass::CreateShader() const
+{
+    HRESULT hr = ShaderManager->AddVertexShader(L"DepthOnlyVS", L"Shaders/DepthOnlyVS.hlsl", "mainVS");
+    if (FAILED(hr))
+    {
+        UE_LOG(LogLevel::Error, TEXT("Failed to create DepthOnlyVS shader!"));
+    }
+}
+
+void FShadowRenderPass::UpdateViewport(const uint32& InWidth, const uint32& InHeight)
+{
+    // Set the viewport
+    D3D11_VIEWPORT shadowViewport;
+    ZeroMemory(&shadowViewport, sizeof(D3D11_VIEWPORT));
+    shadowViewport.TopLeftX = 0;
+    shadowViewport.TopLeftY = 0;
+    shadowViewport.Width = static_cast<float>(InWidth);
+    shadowViewport.Height = static_cast<float>(InHeight);
+    shadowViewport.MinDepth = 0.0f;
+    shadowViewport.MaxDepth = 1.0f;
 }
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/ShadowRenderPass.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/ShadowRenderPass.h
@@ -4,22 +4,42 @@
 #include "Container/Set.h"
 #include "Define.h"
 #include "UnrealClient.h" // Depth Stencil View
+#include <d3d11.h>
+
 
 // ShadowMap을 생성하기 위한 Render Pass입니다.
 // Depth 값만을 추출하는 Vertex Shader Only 패스
 // 모든 Light에 대한 Depth Map Texture를 생성합니다.
 // ViewMode != Unlit일 때에만 Static Mesh Render Pass 에서 실행됩니다
+
+class FDXDBufferManager;
+class FDXDShaderManager;
+class FGraphicsDevice;
+class ULightComponentBase;
+
 class FShadowRenderPass : public IRenderPass
 {
 public:
     FShadowRenderPass();
     virtual ~FShadowRenderPass();
-    virtual void Initialize(FDXDBufferManager* InBufferManager, FGraphicsDevice* InGraphics, FDXDShaderManager* InShaderManager) override;
+    void CreateShader() const;
+    void UpdateViewport(const uint32& InWidth, const uint32& InHeight);
+    void Initialize(FDXDBufferManager* InBufferManager, FGraphicsDevice* InGraphics, FDXDShaderManager* InShaderManager) override;
+    void PrepareRenderState();
     virtual void PrepareRenderArr() override;
     virtual void Render(const std::shared_ptr<FEditorViewportClient>& Viewport) override;
     virtual void ClearRenderArr() override;
 
 private:
+    FDXDBufferManager* BufferManager;
+    FGraphicsDevice* Graphics;
+    FDXDShaderManager* ShaderManager;
+
     ID3D11InputLayout* StaticMeshIL;
     ID3D11VertexShader* DepthOnlyVS;
-}
+
+    D3D11_VIEWPORT ShadowViewport;
+
+    uint32 ShadowMapWidth = 2048;
+    uint32 ShadowMapHeight = 2048;
+};
