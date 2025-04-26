20e98a11c8cf03a0b6b6cb56be90ca5a89fce044
Joochan <joochan123123@gmail.com>
2025-04-26 19:45:21 +0900

    fix: SelecActor는 null체크를 하기에 DeselecActor로 변경.

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/EditorEngine.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/EditorEngine.cpp
@@ -135,8 +135,9 @@ void UEditorEngine::EndPIE()
         PIEWorld = nullptr;
 
         // TODO: PIE에서 EditorWorld로 돌아올 때, 기존 선택된 Picking이 유지되어야 함. 현재는 에러를 막기위해 임시조치.
-        SelectActor(nullptr);
-        SelectComponent(nullptr);
+        
+        DeselectActor(GetSelectedActor());
+        DeselectComponent(GetSelectedComponent());
     }
     // 다시 EditorWorld로 돌아옴.
     ActiveWorld = EditorWorld;
