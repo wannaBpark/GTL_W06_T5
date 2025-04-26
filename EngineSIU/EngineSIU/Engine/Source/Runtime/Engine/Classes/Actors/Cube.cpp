300b3132750cad07bc538cfb6822c0e3236fe9c2
UISOO <zeroapple000@gmail.com>
2025-04-26 13:53:36 +0900

    Add Temp Code (Add Shape Component by UI)

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Actors/Cube.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Actors/Cube.cpp
@@ -1,5 +1,8 @@
 #include "Cube.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/Shapes/BoxComponent.h"
+#include "Components/Shapes/CapsuleComponent.h"
+#include "Components/Shapes/SphereComponent.h"
 
 #include "Engine/FLoaderOBJ.h"
 
@@ -10,6 +13,19 @@ ACube::ACube()
     // Begin Test
     //StaticMeshComponent->SetStaticMesh(FManagerOBJ::GetStaticMesh(L"Contents/helloBlender.obj"));
     StaticMeshComponent->SetStaticMesh(FManagerOBJ::GetStaticMesh(L"Contents/Reference/Reference.obj"));
+
+
+
+    // TODO 임시 코드임. (Class Table에 생성되지 않으면 안넣어짐.)
+    auto BoxComponent = AddComponent<UBoxComponent>();
+    BoxComponent->DestroyComponent();
+
+    auto SphereComponent = AddComponent<USphereComponent>();
+    SphereComponent->DestroyComponent();
+
+    auto CapsuleComponent = AddComponent<UCapsuleComponent>();
+    CapsuleComponent->DestroyComponent();
+    
     // End Test
 }
 
